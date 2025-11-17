aaffe6e5e78e75aa65feba596c8fe6ea79d63ddd
mahfuzaMou71 <mahfuzamou562@gmail.com>
2025-11-17 13:17:24 +0600

    Updated 2.php

--- Mid/Lab Task 2/2.php
+++ Mid/Lab Task 2/2.php
@@ -9,3 +9,17 @@
       background-color: #111827;
       color: #e5e7eb;
     }
+
+    h2, h3 {
+      text-align: center;
+      color: #f9fafb;
+    }
+
+    form {
+      background-color: #0f172a;
+      padding: 20px;
+      border-radius: 12px;
+      width: 340px;
+      margin: 10px auto 0 auto;
+      box-shadow: 0 10px 25px rgba(0,0,0,0.4);
+    }
