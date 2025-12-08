17fa72ad8f325218cb9f7b722a973421e7739f92
mahfuzaMou71 <mahfuzamou562@gmail.com>
2025-12-08 14:04:34 +0600

    added the lab1.php and inserted the code

---
+++ Final/Lab Task 1/lab1.php
@@ -0,0 +1,11 @@
+<?php
+$students = array(
+    "Arman" => 85,
+    "Mahir" => 92,
+    "Samira" => 78
+);
+
+foreach ($students as $name => $score) {
+    echo $name . ": " . $score . "<br>";
+}
+?>
